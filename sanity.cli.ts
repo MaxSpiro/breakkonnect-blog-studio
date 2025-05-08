55424ba2f3e3c01cbebfd0d98f740691d1e2129d
MaxSpiro <maxspiridonov04@gmail.com>
2025-05-08 13:25:48 -0700

    auto deploy host

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'wklqtc88',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'breakkonnect-blog',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
